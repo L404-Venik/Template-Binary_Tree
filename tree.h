712852cce001a9b19d4e6b01e726978f515f8976
Strokov Veniamin <67225529+L404-Venik@users.noreply.github.com>
2024-10-13 12:05:25 +0300

    Add files via upload

---
+++ tree.h
@@ -0,0 +1,123 @@
+#pragma once
+
+#include <memory>
+
+namespace NBinTree {
+
+template <typename T>
+class TNode : public std::enable_shared_from_this<TNode<T>> {
+public:
+    using TNodePtr = std::shared_ptr<TNode<T>>;
+    using TNodeConstPtr = std::shared_ptr<const TNode<T>>;
+
+    bool HasLeft() const {
+        return Left;
+    }
+
+    bool HasRight() const {
+        return Right;
+    }
+
+    bool HasParent() const {
+        return Parent;
+    }
+
+    T& GetValue() {
+        return Value;
+    }
+
+    const T& GetValue() const {
+        return Value;
+    }
+
+    TNodePtr GetLeft() {
+        return Left;
+    }
+
+    TNodeConstPtr GetLeft() const {
+        return Left;
+    }
+
+    TNodePtr GetRight() {
+        return Right;
+    }
+
+    TNodeConstPtr GetRight() const {
+        return Right;
+    }
+
+    TNodePtr GetParent() {
+        return Parent;
+    }
+
+    TNodeConstPtr GetParent() const {
+        return Parent;
+    }
+
+    static TNodePtr CreateLeaf(T value) {
+        return std::make_shared<TNode>(value);
+    }
+
+    static TNodePtr Fork(T value, TNodePtr left, TNodePtr right) {
+        TNodePtr ptr = std::make_shared<TNode>(value, left, right);
+        SetParent(ptr->GetLeft(), ptr);
+        SetParent(ptr->GetRight(), ptr);
+        return ptr;
+    }
+
+    TNodePtr ReplaceLeft(TNodePtr left) {
+        SetParent(left, this->shared_from_this());
+        SetParent(Left, nullptr);
+        std::swap(left, Left);
+        return left;
+    }
+
+    TNodePtr ReplaceRight(TNodePtr right) {
+        SetParent(right, this->shared_from_this());
+        SetParent(Right, nullptr);
+        std::swap(right, Right);
+        return right;
+    }
+
+    TNodePtr ReplaceRightWithLeaf(T value) {
+        return ReplaceRight(CreateLeaf(value));
+    }
+
+    TNodePtr ReplaceLeftWithLeaf(T value) {
+        return ReplaceLeft(CreateLeaf(value));
+    }
+
+    TNodePtr RemoveLeft() {
+        return ReplaceLeft(nullptr);
+    }
+
+    TNodePtr RemoveRight() {
+        return ReplaceRight(nullptr);
+    }
+
+    TNode(T value)
+        : Value(value)
+    {
+    }
+
+    TNode(T value, TNodePtr left, TNodePtr right)
+        : Value(value)
+        , Left(left)
+        , Right(right)
+    {
+    }
+private:
+    T Value;
+    TNodePtr Left = nullptr;
+    TNodePtr Right = nullptr;
+    TNodePtr Parent = nullptr;
+
+
+    static void SetParent(TNodePtr node, TNodePtr parent) {
+        if (node) {
+            node->Parent = parent;
+        }
+    }
+};
+
+} // namespace NBinTree
